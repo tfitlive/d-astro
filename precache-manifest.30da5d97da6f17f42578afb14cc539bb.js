b6fc038e39efa17c443bf5377c3ef71fd5467f45
DOMLIANG <pandaqiang@gmail.com>
2019-07-26 17:26:23 +0800

    Updates

---
+++ precache-manifest.30da5d97da6f17f42578afb14cc539bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c01763efa55622759971074120998ea",
+    "url": "/d-astro/index.html"
+  },
+  {
+    "revision": "77123a1e83f56f9bcf64",
+    "url": "/d-astro/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/d-astro/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "77123a1e83f56f9bcf64",
+    "url": "/d-astro/static/js/main.85299cfe.chunk.js"
+  },
+  {
+    "revision": "cc405ef9b637685ef4d6",
+    "url": "/d-astro/static/js/runtime~main.925aa9f7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/d-astro/static/media/logo.5d5d9eef.svg"
+  }
+]);
