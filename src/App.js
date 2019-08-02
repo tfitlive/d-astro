628b0daef48f5dbf23000ec52ec26944086cbe51
DOMLIANG <pandaqiang@gmail.com>
2019-08-02 12:07:18 +0800

    feat: add link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './appicon.png';
+// import logo from './appicon.png';
 import './App.css';
 
 function App() {
@@ -13,20 +13,28 @@ function App() {
           The most popular astrology app! before you use it you need read the <a href="https://www.tfit.live/support/terms.html">Terms</a> and <a href="https://www.tfit.live/support/privacy.html">Privacy</a>
         </p>
         <a
-          className="App-link"
-          href="#"
+          className="Appstore"
+          href="https://apps.apple.com/us/app/wechat/id1472146987"
           target="_blank"
           rel="noopener noreferrer"
         >
           AppStore
         </a>
         <a
-          className="App-link"
-          href="#"
+          className="GooglePlay"
+          href="https://play.google.com/store/apps/details?id=live.tfit.daily_astrology"
           target="_blank"
           rel="noopener noreferrer"
         >
           GooglePlay
+        </a>
+        or an<a
+          className="Apk"
+          href="https://github.com/tfitlive/d-astro/releases/download/v1.0.3%2B9/app-release.apk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Apk
         </a>
       </header>
     </div>
